acc3452667924f58898093428bdfddf902a02ebf
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2023-01-13 17:45:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def display(name, color='white'):
-    return '"display":{"Name":\'{"text":"' + name + '", "color": "' + color +'"}\'}'
+def display(name, color='white', lore='', is_italic=True):
+    is_italic = str(is_italic).lower()
+    return '"display":{"Name":\'{"text":"' + name + '", "color": "' \
+        + color +'", "italic": "' + is_italic +'"}\', Lore:[\'"' + lore + '"\']}'
 
 
-def give_command(target, item_id, amount, attr_s=''):
-    return f'/give {target} minecraft:{item_id}{attr_s} {amount}'
+def item(item_id, amount, attr_s=''):
+    return f'minecraft:{item_id}{attr_s} {amount}'
 
 
-print(display('frick', 'red'))
+def give_command(target, item):
+    return f'/give {target} item'
+
+
+print(display('frick', 'red', 'lore', False))
 
 #attr = load_dict('buffer.json')
 #print(give_command('@p', 'netherite_sword', attr, 1))
