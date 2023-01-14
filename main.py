e26b6f05669347014488654a5310bc766667457d
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2023-01-14 05:56:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from random import randint, choice
 
 
-def display(name, color='white', lore='', is_italic=True):
+def style(name, color='white', lore='', is_italic=False):
     is_italic = str(is_italic).lower()
+    lore_inject = f', Lore:[\'"{lore}"\']' if lore else ''
     return '"display":{"Name":\'{"text":"' + name + '", "color": "' \
-        + color +'", "italic": "' + is_italic +'"}\', Lore:[\'"' + lore + '"\']}'
+        + color +'", "italic": "' + is_italic +'"}\'' + lore_inject + '}'
+
+
+def enchantments(*ench):
+    rows = []
+    for ench_id, level in ench:
+        row = '{id:' + ench_id + ',lvl:' + str(level) + '}'
+        rows.append(row)
+    return f'Enchantments:[{",".join(rows)}]'
 
 
 def join_attr(*attr):
     return '{' + ', '.join(attr) + '}'
 
 
-def chest(items):
+def chest_inner(items, as_item=True):
     rows = []
     for slot, item_id, count, tag in items:
-        row = f'Slot:{slot},Count:{count},id:{item_id},tag:{tag}'
+        tag_code = f',tag:{tag}' if tag else ''
+        row = f'Slot:{slot},Count:{count},id:{item_id}' + tag_code
         rows.append('{' + row + '}')
-    return 'chest{Items:[' + ','.join(rows) + ']}'
+    a = 'Items:[' + ','.join(rows) + ']'
+    if as_item:
+        return f'BlockEntityTag:{a}'
+    return a
+
+def item(idx, *attr):
+    return idx + f'{join_attr(*attr)}'
+
+#/give @p chest{BlockEntityTag:{Items:[{Slot:0,id:acacia_boat,Count:1}]}} 1
 
 
-COLORS = ['white', 'red', 'yellow', 'blue', 'green']
-ITEMS = ['iron_sword', 'beef', 'iron_shovel', 'grass']
-
-items = [
-    (i, choice(ITEMS), 1, join_attr(display('fuck', color=choice(COLORS), lore='f'))) for i in range(27)
-]
-
-print(chest(items))
+print(item('chest', style('kit', 'red'), chest_inner([
+    (0, 'grass', 2, join_attr(
+        style('Excalibur', 'red', 'Fuck'),
+        enchantments(
+            ('sharpness', 5)
+        )
+    ))
+], False)))
